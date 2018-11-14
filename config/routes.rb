5800bc1aad94b4c71d66a56ef23fe1c0051ef0ce
Timothy Fisher <timothyf@gmail.com>
2018-11-14 17:40:27 -0500

    more app setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+
   #resources :users
+  devise_for :users
+
+  ActiveAdmin.routes(self)
+  #devise_for :users #, ActiveAdmin::Devise.config
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
--- db/fixtures/users.rb
+++ db/fixtures/users.rb
@@ -1,4 +1,4 @@
-Member.seed(:id,
+User.seed(:id,
   {
     :id => 1, :email => 'admin@example.com', :password => 'password', :password_confirmation => 'password', :admin => true
   },
