ac9f18eb5e7aecec709997dd3d1281313fc4ae8b
Timothy Fisher <timothyf@gmail.com>
2018-11-15 13:28:29 -0500

    added bootstrap to template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < InheritedResources::Base
 
+  # def index
+  #
+  # end
+  #
+  # def show
+  #
+  # end
+
   private
 
     def user_params
       params.require(:user).permit()
     end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  #resources :users
   devise_for :users
-
   ActiveAdmin.routes(self)
+  resources :users
   #devise_for :users #, ActiveAdmin::Devise.config
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
